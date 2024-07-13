926f369515cc1ce842ecd91622e06d2a7422a0c5
Dat Bogie <89746947+DatBogie@users.noreply.github.com>
2024-07-12 23:43:13 -0400

    Update index.js added a bagillion number thingies
    OH and I fixed a bug with the Doubly Efficient Auto Clickers
    and I made it so it shows you how many of each shop item you have

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ class shopItem {
         if (x != "") {
             el.innerHTML = x;
         } else {
-            let t = `${this.name} (${readableNum(this.price)}C)`;
+            let t = `${this.name} (${readableNum(this.price)}C) [x${this.owned}]`;
             el.innerHTML = t;
         };
     };
@@ -200,7 +200,7 @@ class shopItem {
 const shopItems = [
     new shopItem("Auto Clicker","+5 CPS",50,1.2,function(){CPS+=(5*CPSV)},"cps1"),
     new shopItem("Super Click","+1 CPC",100,1.4,function(){CPC+=(1*CPCV)},"cpc1"),
-    new shopItem("Doubly Efficient Auto Clickers","Double CPS, +1 CPSV",1000,2,function(){CPS*=2;CPSV+=1},"cps2")
+    new shopItem("Doubly Efficient Auto Clickers","Double CPS, +1 CPSV",1000,2,function(){CPSV+=1;CPS=(CPS/(CPSV-1))*CPSV;},"cps2")
 ];
 
 function openPrompt(title="Prompt",txt0="Confirm",txt1="Cancel",txti="",f0=closePrompt,f1=closePrompt) {
@@ -392,7 +392,98 @@ function readableNum(n = 0) {
         { name: "Sp", value: 1e24 },        // Septillion
         { name: "Oc", value: 1e27 },        // Octillion
         { name: "N", value: 1e30 },         // Nonillion
-        { name: "Dc", value: 1e33 }         // Decillion
+        { name: "Dc", value: 1e33 },        // Decillion
+        { name: "UDc", value: 1e36 },       // Undecillion
+        { name: "DDc", value: 1e39 },       // Duodecillion
+        { name: "TDc", value: 1e42 },       // Tredecillion
+        { name: "QaDc", value: 1e45 },      // Quattuordecillion
+        { name: "QiDc", value: 1e48 },      // Quindecillion
+        { name: "SxDc", value: 1e51 },      // Sexdecillion
+        { name: "SpDc", value: 1e54 },      // Septendecillion
+        { name: "OcDc", value: 1e57 },      // Octodecillion
+        { name: "NDc", value: 1e60 },       // Novemdecillion
+        { name: "Vg", value: 1e63 },        // Vigintillion
+        { name: "UVg", value: 1e66 },       // Unvigintillion
+        { name: "DVg", value: 1e69 },       // Duovigintillion
+        { name: "TVg", value: 1e72 },       // Trevigintillion
+        { name: "QaVg", value: 1e75 },      // Quattuorvigintillion
+        { name: "QiVg", value: 1e78 },      // Quinvigintillion
+        { name: "SxVg", value: 1e81 },      // Sexvigintillion
+        { name: "SpVg", value: 1e84 },      // Septenvigintillion
+        { name: "OcVg", value: 1e87 },      // Octovigintillion
+        { name: "NVg", value: 1e90 },       // Novemvigintillion
+        { name: "Tg", value: 1e93 },        // Trigintillion
+        { name: "UTg", value: 1e96 },       // Untrigintillion
+        { name: "DTg", value: 1e99 },       // Duotrigintillion
+        { name: "TTg", value: 1e102 },      // Tretrigintillion
+        { name: "QaTg", value: 1e105 },     // Quattuortrigintillion
+        { name: "QiTg", value: 1e108 },     // Quintrigintillion
+        { name: "SxTg", value: 1e111 },     // Sextrigintillion
+        { name: "SpTg", value: 1e114 },     // Septentrigintillion
+        { name: "OcTg", value: 1e117 },     // Octotrigintillion
+        { name: "NTg", value: 1e120 },      // Novemtrigintillion
+        { name: "Qd", value: 1e123 },       // Quadragintillion
+        { name: "UQd", value: 1e126 },      // Unquadragintillion
+        { name: "DQd", value: 1e129 },      // Duoquadragintillion
+        { name: "TQd", value: 1e132 },      // Trequadragintillion
+        { name: "QaQd", value: 1e135 },     // Quattuorquadragintillion
+        { name: "QiQd", value: 1e138 },     // Quinquadragintillion
+        { name: "SxQd", value: 1e141 },     // Sexquadragintillion
+        { name: "SpQd", value: 1e144 },     // Septenquadragintillion
+        { name: "OcQd", value: 1e147 },     // Octoquadragintillion
+        { name: "NQd", value: 1e150 },      // Novemquadragintillion
+        { name: "Qn", value: 1e153 },       // Quinquagintillion
+        { name: "UQn", value: 1e156 },      // Unquinquagintillion
+        { name: "DQn", value: 1e159 },      // Duoquinquagintillion
+        { name: "TQn", value: 1e162 },      // Trequinquagintillion
+        { name: "QaQn", value: 1e165 },     // Quattuorquinquagintillion
+        { name: "QiQn", value: 1e168 },     // Quinquinquagintillion
+        { name: "SxQn", value: 1e171 },     // Sexquinquagintillion
+        { name: "SpQn", value: 1e174 },     // Septenquinquagintillion
+        { name: "OcQn", value: 1e177 },     // Octoquinquagintillion
+        { name: "NQn", value: 1e180 },      // Novemquinquagintillion
+        { name: "Sxg", value: 1e183 },      // Sexagintillion
+        { name: "USxg", value: 1e186 },     // Unsexagintillion
+        { name: "DSxg", value: 1e189 },     // Duosexagintillion
+        { name: "TSxg", value: 1e192 },     // Tresexagintillion
+        { name: "QaSxg", value: 1e195 },    // Quattuorsexagintillion
+        { name: "QiSxg", value: 1e198 },    // Quinsexagintillion
+        { name: "SxSxg", value: 1e201 },    // Sexsexagintillion
+        { name: "SpSxg", value: 1e204 },    // Septensexagintillion
+        { name: "OcSxg", value: 1e207 },    // Octosexagintillion
+        { name: "NSxg", value: 1e210 },     // Novemsexagintillion
+        { name: "Spg", value: 1e213 },      // Septuagintillion
+        { name: "USpg", value: 1e216 },     // Unseptuagintillion
+        { name: "DSpg", value: 1e219 },     // Duoseptuagintillion
+        { name: "TSpg", value: 1e222 },     // Treseptuagintillion
+        { name: "QaSpg", value: 1e225 },    // Quattuorseptuagintillion
+        { name: "QiSpg", value: 1e228 },    // Quinseptuagintillion
+        { name: "SxSpg", value: 1e231 },    // Sexseptuagintillion
+        { name: "SpSpg", value: 1e234 },    // Septenseptuagintillion
+        { name: "OcSpg", value: 1e237 },    // Octoseptuagintillion
+        { name: "NSpg", value: 1e240 },     // Novemseptuagintillion
+        { name: "Og", value: 1e243 },       // Octogintillion
+        { name: "UOg", value: 1e246 },      // Unoctogintillion
+        { name: "DOg", value: 1e249 },      // Duooctogintillion
+        { name: "TOg", value: 1e252 },      // Treoctogintillion
+        { name: "QaOg", value: 1e255 },     // Quattuoroctogintillion
+        { name: "QiOg", value: 1e258 },     // Quinoctogintillion
+        { name: "SxOg", value: 1e261 },     // Sexoctogintillion
+        { name: "SpOg", value: 1e264 },     // Septenoctogintillion
+        { name: "OcOg", value: 1e267 },     // Octooctogintillion
+        { name: "NOg", value: 1e270 },      // Novemoctogintillion
+        { name: "Ng", value: 1e273 },       // Nonagintillion
+        { name: "UNg", value: 1e276 },      // Unnonagintillion
+        { name: "DNg", value: 1e279 },      // Duononagintillion
+        { name: "TNg", value: 1e282 },      // Trenonagintillion
+        { name: "QaNg", value: 1e285 },     // Quattuornonagintillion
+        { name: "QiNg", value: 1e288 },     // Quinnonagintillion
+        { name: "SxNg", value: 1e291 },     // Sexnonagintillion
+        { name: "SpNg", value: 1e294 },     // Septennonagintillion
+        { name: "OcNg", value: 1e297 },     // Octononagintillion
+        { name: "NNg", value: 1e300 },      // Novemnonagintillion
+        { name: "C", value: 1e303 },        // Centillion
+        { name: "UC", value: 1e306 }        // Uncentillion
     ];
     let y = "";
     for (let i = largeNumbers.length - 1; i >= 0; i--) {
@@ -413,7 +504,6 @@ function updateCookies(x=0) {
         let r_CPC = readableNum(CPC);
         let r_CPSV = readableNum(CPSV);
         let r_CPCV = readableNum(CPCV);
-        // el.innerHTML = ("Cookies: " + COOKIES.toString() + "C") + "<br>" + ("CPS: " + CPS.toString()) + "<br>" + ("CPC: " + CPC.toString());
         el.innerHTML = `${r_COOKIES}C<br>${r_CPS}C/s<br>${r_CPC}C/c<br>${r_CPSV}xCPS<br>${r_CPCV}xCPC`;
         el.title = `${COOKIES} ${((COOKIES != 1) ? "Clicks" : "Click")} (C)\n${CPS} ${((CPS != 1) ? "Clicks" : "Click")} Per Second (CPS)\n${CPC} ${((CPC != 1) ? "Clicks" : "Click")} Per Click (CPC)\n${CPSV}x CPS Value Multiplier (CPSV)\n${CPCV}x CPC Value Multiplier (CPCV)`;
     };
